f5cac28e298e746c873de311ff445db7e77f1f6e
jay <dj.min43@gmail.com>
2022-04-10 08:32:57 +0900

    array cardio 1

--- 04-cardio/index.js
+++ 04-cardio/index.js
@@ -64,26 +64,64 @@ const people = [
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
-
+const fifteen = inventors.filter(function (inventor) {
+  if (inventor.year >= 1500 && inventor.year <= 1600) {
+    return true;
+  }
+});
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
+const fullNames = inventors.map(
+  (inventor) => inventor.first + " " + inventor.last
+);
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
+const ordered = inventors.sort(function (a, b) {
+  if (a.year > b.year) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
+const orderedShort = inventors.sort((a, b) => (a.year > b.year ? 1 : -1));
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
+const totalYears = inventors.reduce((total, inventor) => {
+  return total + (inventor.passed - inventor.year);
+}, 0);
 
 // 5. Sort the inventors by years lived
+const oldest = inventors.sort(function (a, b) {
+  const lastGuy = a.passed - a.year;
+  const nextGuy = b.passed - b.year;
+  if (lastGuy > nextGuy) {
+    return -1;
+  } else {
+    return 1;
+  }
+});
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+// const category = document.querySelector(".mw-category");
+// const links = [...category.querySelectorAll("a")];
+// const de = links
+//   .map((link) => link.textContent)
+//   .filter((streetName) => streetName.includes("de"));
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
+const alpha = people.sort(function (lastOne, nextOne) {
+  const [aLast, aFirst] = lastOne.split(", ");
+  const [bLast, bFirst] = nextOne.split(", ");
+  return aLast > bLast ? 1 : -1;
+});
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
+
 const data = [
   "car",
   "car",
@@ -100,3 +138,12 @@ const data = [
   "car",
   "truck",
 ];
+
+const sum = data.reduce((memo, newItem) => {
+  if (!memo[newItem]) {
+    memo[newItem] = 0;
+  }
+  memo[newItem]++;
+  return memo;
+}, {});
+console.log(sum);
