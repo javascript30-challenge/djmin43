2e50f8f7d43275fd773551f17f8125374c2fdf48
jay <dj.min43@gmail.com>
2022-04-16 08:04:37 +0900

    i am a console master

--- 09 - Dev Tools Domination/index.js
+++ 09 - Dev Tools Domination/index.js
@@ -10,25 +10,47 @@ function makeGreen() {
 }
 
 // Regular
+console.log("hello");
 
 // Interpolated
+console.log("hello i am %s string", "austin");
 
 // Styled
+console.log("%c i am some great text", "font-size:50px");
 
 // warning!
+console.warn("oh no");
 
 // Error :|
+console.error("shit!");
 
 // Info
+console.info("crocodiles eat 3-4 people per year");
 
 // Testing
+console.assert(1 === 1, "that is wrong");
 
 // clearing
+console.clear();
 
 // Viewing DOM Elements
+const p = document.querySelector("p");
+console.dir(p);
 
 // Grouping together
+dogs.forEach((dog) => {
+  console.group(`${dog.name}`);
+  console.log(`${dog.name} is austin`);
+  console.groupEnd(`${dog.name}`);
+});
 
 // counting
+console.count("Wes");
 
 // timing
+console.time("fetching data");
+fetch("https://api.github.com/users/wesbos")
+  .then((data) => data.json)
+  .then((data) => {
+    console.timeEnd("fetching data");
+  });
